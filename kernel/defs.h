a8e31ed9a79dd656141afb87536173f282f0ca65
zianglei <zianglei@126.com>
2020-10-19 18:50:10 +0800

    copyinstr done

--- kernel/defs.h
+++ kernel/defs.h
@@ -189,6 +189,7 @@ void            vmprint(pagetable_t);
 
 // vmcopyin.c
 int             copyin_new(pagetable_t, char*, uint64, uint64);
+int             copyinstr_new(pagetable_t, char*, uint64, uint64);
 
 // plic.c
 void            plicinit(void);
