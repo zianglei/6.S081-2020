70d1ef1e31a15eccdb8e151a537ddcad6d37d46e
zianglei <zianglei@126.com>
2021-03-01 17:33:21 +0800

    echo works

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -47,8 +47,9 @@ sys_sbrk(void)
   if(argint(0, &n) < 0)
     return -1;
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  // Change the process's memory size but not allocate physical memory
+  myproc()->sz += n;
+
   return addr;
 }
 
